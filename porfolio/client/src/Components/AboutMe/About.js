a2744159518fbc532cb7af71fbacf88728f5ac69
Farouk <faroukkisuule55@gmail.com>
2021-06-02 13:04:11 -0400

    Updated about me section

--- porfolio/client/src/Components/AboutMe/About.js
+++ porfolio/client/src/Components/AboutMe/About.js
@@ -19,8 +19,7 @@ const About = () => {
               <p className="passion">
                 I am passionate about building excellent software that improves
                 the lives of those around me. I specialize in creating software
-                for clients ranging from individuals and small-businesses all
-                the way to large enterprise corporations. What would you do if
+                for clients ranging from individuals and small-businesses. What would you do if
                 you had a software expert available at your fingertips?
               </p>
             </div>
