4878bba642c1f2e70d3fcfb514dac9b735f753ac
Farouk <faroukkisuule55@gmail.com>
2021-06-08 13:47:16 -0400

    Updated Portfolio pages

---
+++ client/src/Components/Home/Home.js
@@ -0,0 +1,66 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from "react";
+import "./Home.css";
+import Slide from "react-reveal/Slide";
+import { NavLink } from "react-router-dom"
+
+const Home = () => {
+  return (
+    <>
+      <header>
+        <div className='container cover'>
+          <div className='row upbox'>
+            <Slide left>
+              <div className='col-lg-6 middle'>
+                <h1 className='about'>
+                  <em className='slide'>f</em>arouk
+                  <strong className='strong'>.is()</strong>
+                </h1>
+
+                <p className='aboutMe'>
+                  /** Full Stack Web Developer specialized in both front-end and
+                  back-end.*/{" "}
+                  <span className='fof'>
+                    <em></em>
+                  </span>
+                  {/* <span classame="fof"></span> */}
+                  <br></br>
+                  <br></br>
+                  {/* <div class='btn-group' role='group' aria-label='Basic example'> */}
+                  {/* <button type='button' class='btn btn-outline-success'>
+                    Portfolio
+                  </button> */}
+                  {/* <a href='/'>
+                      <button className=' button-3 px-5'>View</button>
+                    </a> */}
+                  {/* <button type='button' class='btn btn-primary'>
+                Middle
+              </button>
+              <button type='button' class='btn btn-primary'>
+                Right
+              </button> */}
+                  {/* </div> */}
+                </p>
+              </div>
+            </Slide>
+            <Slide right>
+              <div className='col-lg-6'>
+                <img src='./me.png' alt='me' className='image'></img>
+              </div>
+            </Slide>
+          </div>
+          {/* <hr classNme='mt-0'></hr> */}
+          <div className='row container align'>
+          <div className="col-lg-12">
+            <NavLink to='/about'>
+              <button className=' button-3 px-5'>View</button>
+            </NavLink>
+          </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Home;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended : true} ));
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>{
+    console.log(" ===> Listening to PORT " + PORT)
+});
